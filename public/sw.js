629509d2d050e793f56e7a2f30e69e300984629a
Fahrul Hamzidan Pulungan <145125810+z1dan820@users.noreply.github.com>
2026-01-02 19:34:14 +0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'barberlog-v1';
+const assets = ['/'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
