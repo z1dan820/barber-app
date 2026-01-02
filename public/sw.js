8fee796e7c8bc40f74d3506d758da8dd00993de4
Fahrul Hamzidan Pulungan <145125810+z1dan820@users.noreply.github.com>
2026-01-02 19:43:09 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,24 @@
 const CACHE_NAME = 'barberlog-v1';
-const assets = ['/'];
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
